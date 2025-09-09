ebf36364ca5cc07db31a60a3c49894502e3c028a
evarasvb <evaras@firmavb.cl>
2025-09-08 20:48:15 -0700

    fix(jobs): add sys.path to update_marketing

--- jobs/update_marketing.py
+++ jobs/update_marketing.py
@@ -1,5 +1,8 @@
 # jobs/update_marketing.py
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import pandas as pd
 from datetime import datetime
 from app.gsheet_helper import upsert_tab
